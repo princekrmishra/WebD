e2a075aa6107d69562c26999324acb0b7679f45d
Prince kumar mishra <kmishraprince@gmail.com>
2024-08-06 01:18:42 +0530

    Modern Button

---
+++ JavaScript/Basic 1/index.js
@@ -0,0 +1,24 @@
+
+console.log("Namaste Duniya 2");
+
+let a = 5;
+console.log(a);
+var b = 6;
+console.log(b);
+var b = 8;
+console.log(b); 
+
+let c;
+console.log(c); // undefined
+
+let lastName = "prince";
+console.log(lastName);
+
+lastName = 5;
+console.log(lastName); // 5
+
+let age = 27;
+
+let status = (age >= 18) ? 'Yes vote ' : 'No vote';
+
+console.log(status);
