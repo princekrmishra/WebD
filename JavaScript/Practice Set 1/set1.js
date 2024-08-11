be241d985a03bd8d1951a6e576b0e9348803dad7
Prince kumar mishra <kmishraprince@gmail.com>
2024-08-11 23:22:41 +0530

    js - basic

---
+++ JavaScript/Practice Set 1/set1.js
@@ -0,0 +1,20 @@
+//question 1:
+var fname="Prince";
+console.log(fname);
+console.log(typeof(fname));
+var fname=3;
+console.log(fname);
+console.log(typeof(fname));
+
+//question 3
+const item= {
+    things: "LED",
+    price:30
+}
+console.log(item.things);
+console.log(item.price);
+
+const price = 33;
+console.log(price);
+var price=40;
+
